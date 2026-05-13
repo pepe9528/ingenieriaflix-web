b15a8d7fe6eb25debcfdf60473ec0c40e77bce5b
pepe9528 <jose.valdivia0055@alumnos.udg.mx>
2026-05-13 07:18:48 -0600

    0.2

--- Login/js/firebase-config.js
+++ Login/js/firebase-config.js
@@ -1,14 +1,20 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+// Importar funciones necesarias
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
+// Configuración de tu proyecto Firebase
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
+  apiKey: "AIzaSyDRn856EAdCrVxH8FbOUkFemTB1dFv_iqY",
   authDomain: "ingenieriaflix.firebaseapp.com",
   projectId: "ingenieriaflix",
-  storageBucket: "ingenieriaflix.firebasestorage.app",
+  storageBucket: "ingenieriaflix.appspot.com", // 👈 corrige aquí, no "firebasestorage.app"
   messagingSenderId: "129261287747",
-  appId: "1:129261287747:web:ac664370ba8f8bec8b97f6"
+  appId: "1:129261287747:web:ac664370ba8f8bec8b97f6",
+  measurementId: "G-9YQS6QNTR7"
 };
 
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
+
+// Exportar Auth para usarlo en login, registro, etc.
 export const auth = getAuth(app);
