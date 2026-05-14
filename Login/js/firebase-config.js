4c4623643dc9ce5c5c89288c5053ca97869f2c26
pepe9528 <jose.valdivia0055@alumnos.udg.mx>
2026-05-13 21:09:43 -0600

    vvvvvvv

--- Login/js/firebase-config.js
+++ Login/js/firebase-config.js
@@ -1,12 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getRedirectResult } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 // Configuración de tu proyecto Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDRn856EAdCrVxH8FbOUkFemTB1dFv_iqY",
-  authDomain: "silver-wisp-f33951.netlify.app",
+  authDomain: "ingenieriaflix.firebaseapp.com",
   projectId: "ingenieriaflix",
-  storageBucket: "ingenieriaflix.appspot.com", // 👈 corrige aquí, no "firebasestorage.app"
+  storageBucket: "ingenieriaflix.appspot.com",
   messagingSenderId: "129261287747",
   appId: "1:129261287747:web:ac664370ba8f8bec8b97f6",
   measurementId: "G-9YQS6QNTR7"
@@ -14,6 +15,7 @@ const firebaseConfig = {
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-
-// Exportar Auth para usarlo en login, registro, etc.
 export const auth = getAuth(app);
+
+// Limpiar cualquier redirect pendiente de Google/GitHub
+getRedirectResult(auth).catch(() => {});
